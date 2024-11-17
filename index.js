21aef5c2431500aaec759eee35295e7264936f77
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-11-18 01:57:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,50 +18,57 @@ var cookieParser = require('cookie-parser');
 var createError = require('http-errors')
 require('./settings')
 
+cors = require('cors');
 
-cors = require('cors'),
-
-
-    secure = require('ssl-express-www');
+secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')))
+// Active code: Middleware setup and routes
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 var main = require('./routes/main'),
-    api = require('./routes/api')
+    api = require('./routes/api');
 
 app.set('trust proxy', true);
-app.set("json spaces",2)
-app.use(cors())
-app.use(secure)
+app.set("json spaces", 2);
+app.use(cors());
+app.use(secure);
 app.use(cookieParser());
-app.use(express.static("public"))
-app.use('/', main)
-app.use('/api', api)
+app.use(express.static("public"));
+app.use('/', main);
+app.use('/api', api);
 
+// Handle 404 errors
 app.use(function (req, res, next) {
-	next(createError(404))
-  })
+    next(createError(404));
+});
 
+// Error handler middleware
 app.use(function (err, req, res, next) {
-	
-	res.sendFile(__path + '/view/404.html')
-  })
+    res.sendFile(__path + '/view/404.html');
+});
 
-
+// Server startup
 app.listen(PORT, () => {
     console.log(`
-	██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ██╗
-	██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
-	██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝██║
-	██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
-	██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
-	╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
-			                              Maded by Brasho kish 
-								 
- Server running on http://localhost:` + PORT)
-console.log(`Hello ${creator}`)
-})
+    ██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ██╗
+    ██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
+    ██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝██║
+    ██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
+    ██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
+    ╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
+    Maded by Brasho kish 
 
-module.exports = app
+    Server running on http://localhost:` + PORT);
+    console.log(`Hello ${creator}`);
+});
 
+// Inactive code - Example for code you might not need right now
+// Comment out or conditionally include this part based on the environment or feature flag
+/*
+app.get('/inactive', (req, res) => {
+    res.send('This feature is not active');
+});
+*/
+
+module.exports = app;
